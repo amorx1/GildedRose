aace14673e2cf07f3b1143cdc2968c128340e5f9
A Mor <akshay.morx@gmail.com>
2024-07-29 10:15:39 +1200

    factor out BackstagePasses

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -46,32 +46,21 @@ namespace GildedRose.Console
                     if (item.Quality < 50) item.Quality++;
                     item.SellIn--;
                     if (item.Quality < 50 && item.SellIn < 0) item.Quality++;
+                    
                     continue;
                 }
                 
                 if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality++;
+                    if (item.Quality < 50) item.Quality++;
+                    if (item.Quality < 50 && item.SellIn < 11) item.Quality++;
+                    if (item.Quality < 50 && item.SellIn < 6) item.Quality++;
+                    item.SellIn--;
+                    if (item.SellIn < 0) item.Quality = 0;
 
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality++;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality++;
-                            }
-                        }
-                    }
+                    continue;
                 }
+                
                 else
                 {
                     if (item.Quality > 0)
@@ -84,16 +73,9 @@ namespace GildedRose.Console
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality > 0)
                     {
-                        item.Quality = 0;
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality--;
-                        }
+                        item.Quality--;
                     }
                 }
             }
